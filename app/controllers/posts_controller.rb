cf9fc85e6c21e7b9daf525f9b049f5291958d731
AFoo97 <arthur.foozb@gmail.com>
2025-07-27 12:28:45 +1000

    Restricting post creation to signed-in users

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,26 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+
   def new
+    @post = Post.new
   end
 
   def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      redirect_to posts_path, notice: "Post created!"
+    else
+      render :new
+    end
   end
 
   def index
+    @posts = Post.all.includes(:user)
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content)
   end
 end
